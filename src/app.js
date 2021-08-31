840016fd26255328cce93159d6edb755bc834b61
Erik Sievers <erik.sievers@helo.se>
2021-08-31 20:34:43 +0200

    init commit, solved 2 so far

--- src/app.js
+++ src/app.js
@@ -1,21 +1,93 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const { sequelize } = require('./model')
+const { Op } = require('sequelize');
+const { getProfile } = require('./middleware/getProfile')
 const app = express();
+
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
 /**
- * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
+app.get('/contracts/:id', getProfile, async (req, res) => {
+    const { Contract } = req.app.get('models')
+    const { id } = req.params
+    const contract = await Contract.findOne({
+        where: {
+            [Op.and]: [
+                { id },
+                {
+                    [Op.or]: [
+                        { ContractorId: req.profile.id },
+                        { ClientId: req.profile.id }
+                    ]
+                }
+            ]
+
+        }
+    })
+    if (!contract) return res.status(404).end()
     res.json(contract)
 })
+
+/**
+ * @returns all non-terminated contracts for a user
+ */
+app.get('/contracts', getProfile, async (req, res) => {
+    const { Contract } = req.app.get('models')
+    const contracts = await Contract.findAll({
+        where: {
+            [Op.and]: [
+                {
+                    [Op.not]: {
+                        status: 'terminated'
+                    }
+                },
+                {
+                    [Op.or]: [
+                        { ContractorId: req.profile.id },
+                        { ClientId: req.profile.id }
+                    ]
+                }
+            ]
+
+        }
+    })
+    res.json(contracts)
+})
+
+/**
+ * @returns all unpaid jobs for a user's active (non-terminated) contracts
+ */
+app.get('/jobs/unpaid', getProfile, async (req, res) => {
+    const { Job, Contract, Profile } = req.app.get('models')
+    const jobs = await Job.findAll({
+        where: {
+            paid: false
+        },
+        include: {
+            model: Contract,
+            required: true,
+            where: {
+                [Op.and]: [{
+                    [Op.not]: {
+                        status: 'terminated'
+                    }
+                },
+                {
+                    [Op.or]: [
+                        { ClientId: req.profile.id },
+                        { ContractorId: req.profile.id }
+                    ]
+                }]
+            }
+        },
+    })
+    res.json(jobs)
+})
+
+
 module.exports = app;
